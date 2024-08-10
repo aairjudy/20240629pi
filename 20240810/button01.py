4af4e15b733599fccdd7d1566588e889de1f1c13
aairjudy <aairjudy@gmail.com>
2024-08-10 15:56:12 +0800

    update button1

--- 20240810/button01.py
+++ 20240810/button01.py
@@ -1,10 +1,18 @@
 import signal
 from gpiozero import Button,LED
+from datetime import datetime
 
 def user_release():
-    print("使用者按下放開")
+    print("使用者按下放開" ,end = '\t')
     led.toggle()
-    
+    now = datetime.now()
+    strNow =now.strftime('%Y-%m-%d %H:%M:%S')
+    print (strNow ,end = '\t')
+
+    if led.is_lit:
+        print ('開啟電燈中')
+    else:
+        print ('電燈已關閉')
 
 if __name__ == '__main__':
     button = Button(pin=18)
